52b5f4ba1a9554f46b1d313f3c8dd8364f7a4311
manvc <maanvi.leo@gmail.com>
2021-05-26 22:12:09 +0530

    second commit

--- tiny_love_urls_util.py
+++ tiny_love_urls_util.py
@@ -3,20 +3,50 @@ import pandas as pd
 import requests
 import time
 from functools import lru_cache
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env')
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 @lru_cache #its a decorator. Call decorator using @
 def get_nyt_tinylove_urls(return_urls, return_pubdate):
-    seconds = 7
-    urls    = []
-    pubdate = []
-    for page_no in range(14):
-        api_url    = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fl=web_url%2C%20pub_date&fq=headline%3A(%22Tiny%20Love%20Stories%22)&page={}&q=tiny-modern-Love-stories&sort=newest&api-key=b1sSZOCjfGfJQDoU2o2pW5ytwgTnmQ8Y".format(page_no)
-        #print(api_url)
+
+    seconds         = 7
+    urls            = []
+    pubdate         = []
+    
+    
+    dummy_api_url   = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fl=web_url%2C%20pub_date&fq=headline%3A(%22Tiny%20Love%20Stories%22)&page=0&q=tiny-modern-Love-stories&sort=newest&api-key={SECRET_KEY}".format(SECRET_KEY=SECRET_KEY)
+    dummy_link      = requests.get(dummy_api_url)
+    dummy_link_json = dummy_link.json()
+    meta_hits       = dummy_link_json['response']['meta']['hits']
+    # api_latest_date = dummy_link_json['response']['docs'][0]['pub_date']
+    # latest_date     = api_latest_date[0:10]
+    if (meta_hits % 10) == 0:
+        pages = (meta_hits // 10)
+    if (meta_hits % 10) > 0:
+        pages = (meta_hits // 10) + 1
+     
+    # read_df = pd.read_csv('C:/Users/Madhuri/Documents/Python/Project - TLS/tinystories.csv')
+    # csv_latest_date = read_df['Published Date'].iloc[0] 
+
+    for page_no in range(pages):
+        # if latest_date == csv_latest_date:
+        #     break
+        # else:
+        api_url    = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fl=web_url%2C%20pub_date&fq=headline%3A(%22Tiny%20Love%20Stories%22)&page={page_no}&q=tiny-modern-Love-stories&sort=newest&api-key={SECRET_KEY}".format(page_no=page_no, SECRET_KEY=SECRET_KEY)
         links      = requests.get(api_url, time.sleep(seconds))
         links_json = links.json()
+        # meta_hits   = links_json['response']['meta']['hits']
+        # meta_offset = links_json['response']['meta']['offset']
+        # if (meta_hits - meta_offset) < 0:
+        #     break
+        # else:
         for docs in links_json['response']['docs']:
             url             = docs['web_url']
-            published_date = docs['pub_date']
+            published_date  = docs['pub_date']
             urls.append(url)
             pubdate.append(published_date)
     urls_dict = {'Tiny Love Stories URLs': urls, 'Published Date': pubdate}
@@ -28,8 +58,3 @@ def get_nyt_tinylove_urls(return_urls, return_pubdate):
         return(urls)
     elif return_urls == False and return_pubdate == True:
         return(pubdate)
-
-
-
-
-
--- tinylovestories.py
+++ tinylovestories.py
@@ -8,34 +8,45 @@ session = HTMLSession()
 def get_nyt_article(session, tls_url):
     nyt = session.get(tls_url)
     return nyt
-    
+
 tls_url_list, tls_pubdate_list  = get_nyt_tinylove_urls(return_urls=True, return_pubdate=True) #unpacking 
 #tls_pubdate_list = get_nyt_tinylove_urls(return_urls=False, return_pubdate=True)
+
+api_latest_date = tls_pubdate_list[0]
+latest_date     = api_latest_date[0:10]
+read_df = pd.read_csv('C:/Users/Madhuri/Documents/Python/Project - TLS/tinystories.csv')
+if read_df.empty == False:
+    csv_latest_date = read_df['Published Date'].iloc[0]
+else:
+    csv_latest_date = ""
+print(latest_date, csv_latest_date)
 
 publishdatetime = []
 subheading      = []
 paragraph       = []
 writtenby       = []
 
-
-for url,pubdate in zip(tls_url_list,tls_pubdate_list) :
+for url,pubdate in zip(tls_url_list,tls_pubdate_list):
     tls_url        = url
     nyt_data       = get_nyt_article(session=session, tls_url=tls_url)
     stories        = nyt_data.html.find('div.css-1fanzo5.StoryBodyCompanionColumn > div.css-53u6y8', first = False)
 
     for story in stories:
-        sub_heading     = story.find('h2.css-ow6j0y.eoo0vm40', first = True)
-        story_paragraph = story.find('p.css-axufdj.evys1bk0', first = True)
-        author          = story.find('em.css-2fg4z9.e1gzwzxm0', first = True)
-        if sub_heading != None and story_paragraph != None and author != None:
-            subheading.append(sub_heading.text)
-            paragraph.append(story_paragraph.text)
-            writtenby.append(author.text)
-            publishdatetime.append(pubdate)
+        if csv_latest_date != "" and latest_date == csv_latest_date:
+            break
+        else :
+            sub_heading     = story.find('h2.css-ow6j0y.eoo0vm40', first = True)
+            story_paragraph = story.find('p.css-axufdj.evys1bk0', first = True)
+            author          = story.find('em.css-2fg4z9.e1gzwzxm0', first = True)
+            if sub_heading != None and story_paragraph != None and author != None:
+                subheading.append(sub_heading.text)
+                paragraph.append(story_paragraph.text)
+                writtenby.append(author.text)
+                publishdatetime.append(pubdate)
+    #pubdate.date()
     #print(len(publishdatetime), len(subheading), len(paragraph), len(writtenby)) 
-    story_dict = {'Published Date': publishdatetime, 'Story Name':subheading, 'Story':paragraph, 'Author':writtenby}
+    story_dict = {'Published Date':publishdatetime, 'Story Name':subheading, 'Story':paragraph, 'Author':writtenby}
     df = pd.DataFrame(story_dict)
-    df['publishdatetime'] = pd.to_datetime(df.publishdatetime)
-    df['publishdate'] = df['publishdatetime'].dt.strftime['%m%d%y']
-    df.to_csv('tinystories.csv')
-
+    df['Published Date'] = pd.to_datetime(df['Published Date'])
+    df['Published Date'] = df['Published Date'].dt.date
+    df.to_csv('C:/Users/Madhuri/Documents/Python/Project - TLS/tinystories.csv')
